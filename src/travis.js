73da5e2a2c7d880e11774974320094d41ada08bd
Markus Felten <Markus.Felten@gmx.de>
2016-10-28 22:26:12 +0200

    fix: handle empty target

--- src/travis.js
+++ src/travis.js
@@ -3,7 +3,7 @@
 const yaml = require('js-yaml');
 
 export default function (target, template, options = {}) {
-  const yml = yaml.safeLoad(target);
+  const yml = yaml.safeLoad(target) || {};
   const tyml = yaml.safeLoad(template);
 
   Object.keys(tyml).forEach(name => {
