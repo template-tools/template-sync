c0fa18a1b67567c51944d2a07335d7e633acd716
Markus Felten <Markus.Felten@gmx.de>
2016-11-05 23:45:57 +0100

    test: new license test

---
+++ tests/license_test.js
@@ -0,0 +1,14 @@
+/* global describe, it, xit, before, beforeEach, after, afterEach */
+/* jslint node: true, esnext: true */
+
+'use strict';
+
+const chai = require('chai'),
+  assert = chai.assert,
+  expect = chai.expect,
+  should = chai.should(),
+  license = require('../src/license');
+
+describe('modify year', () => {
+  it('should fail', () => 1);
+});
