78c40efea00139686c68a07f1d1f2111a18250b6
Markus Felten <Markus.Felten@gmx.de>
2016-11-07 10:36:02 +0100

    fix: make bin/npm-template-sync executable

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,7 @@ import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 
 export default {
+  banner: '#!/usr/bin/env node',
   format: 'cjs',
   plugins: [nodeResolve(), commonjs()]
 };
