beb1561d98bd1cdd8810b124faaf8929abc949f6
Markus Felten <Markus.Felten@gmx.de>
2016-11-28 23:31:22 +0100

    fix: try to save notification email

--- src/travis.js
+++ src/travis.js
@@ -7,7 +7,13 @@ export default function (target, template, context, options = {}) {
   const yml = yaml.safeLoad(target) || {};
   const tyml = yaml.safeLoad(context.expand(template));
 
+  const email = yml.notifications.email;
+
   deepExtend(yml, tyml);
+
+  if (email) {
+    yml.notifications.email = email;
+  }
 
   return yaml.safeDump(yml);
 }
