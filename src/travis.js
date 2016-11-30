20f7211612074a3881a8fc61f3006e92936e60f7
Markus Felten <Markus.Felten@gmx.de>
2016-11-30 23:55:41 +0100

    fix(travis.yml): merge before_script

--- src/travis.js
+++ src/travis.js
@@ -7,12 +7,22 @@ export default function (target, template, context, options = {}) {
   const yml = yaml.safeLoad(target) || {};
   const tyml = yaml.safeLoad(context.expand(template));
 
+  const before_script = yml.before_script;
+
   const email = yml.notifications.email;
 
   deepExtend(yml, tyml);
 
   if (email) {
     yml.notifications.email = email;
+  }
+
+  if (before_script) {
+    before_script.forEach(s => {
+      if (!yml.before_script.find(e => e === s)) {
+        yml.before_script.push(s);
+      }
+    });
   }
 
   return yaml.safeDump(yml);
