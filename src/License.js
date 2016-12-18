b6ead0356e37030d81f2a3c1d1582e0590eb5951
Markus Felten <Markus.Felten@gmx.de>
2016-12-18 21:31:28 +0100

    fix: sort license years

--- src/License.js
+++ src/License.js
@@ -5,19 +5,18 @@ export default class License extends File {
   get mergedContent() {
     return Promise.all([this.originalContent(), this.templateContent()]).then(contents => {
       const [original, template] = contents;
-      const m = original.match(/opyright\s*\(c\)\s*(\d+)([,\-]\d+)*(\s*(,|by)\s*(.*))?/);
+      const m = original.match(/opyright\s*\(c\)\s*(\d+)([,\-\d]+)*(\s*(,|by)\s*(.*))?/);
       const properties = this.context.properties;
 
       if (m) {
-        const years = {};
-
-        years[properties['date.year']] = properties['date.year'];
-        years[m[1]] = m[1];
+        const years = new Set();
+        years.add(properties['date.year']);
+        years.add(parseInt(m[1]));
 
         if (m[2] !== undefined) {
           m[2].split(/\s*[,\-]\s*/).forEach(y => {
             if (y.length > 0) { 
-              years[y] = y;
+              years.add(parseInt(y));
             }
           });
         }
@@ -26,11 +25,11 @@ export default class License extends File {
           properties['license.owner'] = m[5];
         }
 
-        properties['date.year'] = Object.keys(years).join(',');
+        properties['date.year'] = Array.from(years).sort((a, b) => a < b ? -1 : a > b ? 1 : 0).join(',');
       }
 
       if (original !== '') {
-        return original.replace(/opyright\s*\(c\)\s*(\d+)([,\-]\d+)?/,
+        return original.replace(/opyright\s*\(c\)\s*(\d+)([,\-\d])*/,
           `opyright (c) ${properties['date.year']}`);
       }
 
--- tests/license_test.js
+++ tests/license_test.js
@@ -11,19 +11,39 @@ import License from '../src/License';
 import Client from './Client';
 
 describe('modify year', () => {
-  const context = new Context(new Client({
-    'aFile': {
-      templateRepo: 'Copyright (c) {{date.year}} by {{owner}}',
-      targetRepo: 'Copyright (c) 1999 by xyz'
-    }
-  }), 'targetRepo', 'templateRepo', {
-    'date.year': 2099,
-    owner: 'xyz'
+  describe('one year', () => {
+    const context = new Context(new Client({
+      'aFile': {
+        templateRepo: 'Copyright (c) {{date.year}} by {{owner}}',
+        targetRepo: 'Copyright (c) 1999 by xyz'
+      }
+    }), 'targetRepo', 'templateRepo', {
+      'date.year': 2099,
+      'license.owner': 'xyz'
+    });
+
+    const license = new License(context, 'aFile');
+
+    it('year range', () =>
+      license.mergedContent.then(c =>
+        assert.equal(c, 'Copyright (c) 1999,2099 by xyz')));
   });
 
-  const license = new License(context, 'aFile');
+  describe('year list', () => {
+    const context = new Context(new Client({
+      'aFile': {
+        templateRepo: 'Copyright (c) {{date.year}} by {{owner}}',
+        targetRepo: 'Copyright (c) 2001,1999,2000,2001,2007 by xyz'
+      }
+    }), 'targetRepo', 'templateRepo', {
+      'date.year': 2099,
+      'license.owner': 'xyz'
+    });
 
-  it('year range', () =>
-    license.mergedContent.then(c =>
-      assert.equal(c, 'Copyright (c) 1999,2099 by xyz')));
+    const license = new License(context, 'aFile');
+
+    it('year range', () =>
+      license.mergedContent.then(c =>
+        assert.equal(c, 'Copyright (c) 1999,2000,2001,2007,2099 by xyz')));
+  });
 });
