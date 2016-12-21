86c74351adfefa7267ac4af61e5d6a85b9e95e1d
Markus Felten <Markus.Felten@gmx.de>
2016-12-21 12:45:00 +0100

    feat(Readme): generate badges from package template section

--- src/Readme.js
+++ src/Readme.js
@@ -3,16 +3,24 @@ import File from './File';
 
 export default class Readme extends File {
   get mergedContent() {
-    return Promise.all([this.originalContent(), this.templateContent()]).then(contents => {
-      const [original, template] = contents;
+    return Promise.all([
+        this.originalContent(),
+        this.templateContent(),
+        this.context.files.get('package.json').templateContent()
+      ])
+      .then(contents => {
+        const [original, template, pkg] = contents;
+        const p = JSON.parse(pkg);
+        const badges = p.template.badges.map(b =>
+          this.context.expand(`[![${b.name}](${b.icon})](${b.url})`)
+        );
 
-      const tLines = this.context.expand(template).split(/\n/);
-      const badges = tLines.slice(0, tLines.findIndex(l => l.length === 0));
-      const lines = original.split(/\n/);
-      const fel = lines.findIndex(l => l.length === 0);
+        const tLines = this.context.expand(template).split(/\n/);
+        const lines = original.split(/\n/);
+        const fel = lines.findIndex(l => l.length === 0);
 
-      return badges.join('\n') + '\n' +
-        lines.slice(fel).join('\n');
-    });
+        return badges.join('\n') + '\n' +
+          lines.slice(fel).join('\n');
+      });
   }
 }
