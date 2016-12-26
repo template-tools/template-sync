e77c97dedeb30e06fbf7c3f0b208aa4e893d049c
Markus Felten <Markus.Felten@gmx.de>
2016-12-26 18:41:00 +0100

    playing around with now & github-webhook-handler

---
+++ webhook/index.js
@@ -0,0 +1,32 @@
+/* jslint node: true, esnext: true */
+
+const micro = require('micro'),
+  createHandler = require('github-webhook-handler');
+
+const handler = createHandler({
+  path: '/webhook',
+  secret: 'dfgkjd&dfh'
+});
+
+const server = micro(async(req, res) => {
+
+  handler(req, res, (err) => {
+    res.statusCode = 404;
+    res.end('no such location');
+  });
+
+  res.writeHead(200);
+  res.end('woot');
+});
+
+handler.on('error', function (err) {
+  console.error('Error:', err.message);
+});
+
+handler.on('push', function (event) {
+  console.log('Received a push event for %s to %s',
+    event.payload.repository.name,
+    event.payload.ref);
+});
+
+server.listen(3000);
