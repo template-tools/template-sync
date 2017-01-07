172bf5f0930a7c434c23d6b46ff33ccb9f43f0cd
Markus Felten <Markus.Felten@gmx.de>
2017-01-07 22:31:10 +0100

    refactor: remove unused nodify()

--- src/github.js
+++ src/github.js
@@ -2,13 +2,27 @@
 
 'use strict';
 
-const githubBasic = require('github-basic');
+const github = require('github-basic');
 
 export function pull(from, to, msg, options) {
-  return githubBasic.json('post', `/repos/${to.user}/${to.repo}/pulls`, {
+  return github.json('post', `/repos/${to.user}/${to.repo}/pulls`, {
     title: msg.title,
     body: msg.body,
     base: from.branch,
     head: to.branch
-  }, options).nodeify();
+  }, options);
 }
+
+/*
+export function branch(user, repo, from, to, options) {
+  return github.json('get', '/repos/:owner/:repo/git/refs/:ref', {owner: user, repo: repo, ref: 'heads/' + from}, options)
+    .then(res  =>
+       github.json('post', '/repos/:owner/:repo/git/refs', {
+        owner: user,
+        repo: repo,
+        ref: 'refs/heads/' + to,
+        sha: res.body.object.sha
+      }, options)
+    );
+}
+*/
