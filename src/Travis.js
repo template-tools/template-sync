9e4fbc38763593e271a2a9dd3a91fcb473d5f8f2
Markus Felten <Markus.Felten@gmx.de>
2017-01-16 21:37:19 +0100

    fix(travis): lineWidth=128

--- src/Travis.js
+++ src/Travis.js
@@ -10,33 +10,35 @@ export default class Travis extends File {
   get merge() {
     return Promise.all([this.originalContent(), this.templateContent()])
       .then(([original, template]) => {
-      const yml = yaml.safeLoad(original) || {};
-      const tyml = yaml.safeLoad(this.context.expand(template));
-      const before_script = yml.before_script;
+        const yml = yaml.safeLoad(original) || {};
+        const tyml = yaml.safeLoad(this.context.expand(template));
+        const before_script = yml.before_script;
 
-      const email = yml.notifications.email;
+        const email = yml.notifications.email;
 
-      deepExtend(yml, tyml);
+        deepExtend(yml, tyml);
 
-      if (email) {
-        yml.notifications.email = email;
-      }
+        if (email) {
+          yml.notifications.email = email;
+        }
 
-      if (before_script) {
-        before_script.forEach(s => {
-          if (!yml.before_script.find(e => e === s)) {
-            yml.before_script.push(s);
-          }
+        if (before_script) {
+          before_script.forEach(s => {
+            if (!yml.before_script.find(e => e === s)) {
+              yml.before_script.push(s);
+            }
+          });
+        }
+
+        const content = yaml.safeDump(yml, {
+          lineWidth: 128
         });
-      }
-
-      const content = yaml.safeDump(yml);
-      return {
-        path: this.path,
-        content: content,
-        changed: content != original,
-        message: `chore(travis): merge from template ${this.path}`
-      };
-    });
+        return {
+          path: this.path,
+          content: content,
+          changed: content != original,
+          message: `chore(travis): merge from template ${this.path}`
+        };
+      });
   }
 }
