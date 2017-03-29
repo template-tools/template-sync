dda6717c73943123ea43dab10ec8ed55a64b5d52
Markus Felten <Markus.Felten@gmx.de>
2017-03-29 20:31:13 +0200

    test: check travis

---
+++ tests/travis_test.js
@@ -0,0 +1,32 @@
+/* global describe, it, xit, before, beforeEach, after, afterEach */
+/* jslint node: true, esnext: true */
+
+'use strict';
+
+const assert = require('chai').assert;
+
+import Context from '../src/Context';
+import Travis from '../src/Travis';
+
+import Client from './Client';
+
+describe('merge travis', () => {
+  const context = new Context(new Client({
+    'aFile': {
+      templateRepo: `node_js:
+  - 7.7.2
+`,
+      targetRepo: `node_js:
+  - 7.7.1
+`
+    }
+  }), 'targetRepo', 'templateRepo', {});
+
+  const merger = new Travis(context, 'aFile');
+
+  it('node_js versions', () =>
+    merger.merge.then(m =>
+      assert.equal(m.content, `node_js:
+  - 7.7.2
+`)));
+});
