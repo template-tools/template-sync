03213875ec74dff408c066a5db1192fb006fd4fd
Markus Felten <Markus.Felten@gmx.de>
2017-06-17 17:43:49 +0200

    chore(rollup): make use of pkg

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,13 @@
 import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
+import pkg from './package.json';
 
 export default {
   banner: '#!/usr/bin/env node',
-  format: 'cjs',
-  plugins: [nodeResolve(), commonjs()]
+  targets: [{
+    dest: pkg.bin['npm-template-sync'],
+    format: 'cjs'
+  }],
+  plugins: [nodeResolve(), commonjs()],
+  external: []
 };
