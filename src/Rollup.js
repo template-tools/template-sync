185d62453e663bca8c3388cc54c99bcda070ecfc
Markus Felten <Markus.Felten@gmx.de>
2017-08-20 22:12:03 +0200

    fix(rollup): targets->output, entry->input

--- src/Rollup.js
+++ src/Rollup.js
@@ -14,8 +14,26 @@ export default class Rollup extends File {
     ]).then(([original, templateRaw]) => {
       const ast = recast.parse(original);
 
-      const exp = ast.program.body[2].declaration.properties;
-      console.log(exp);
+      //console.log(ast.program.body);
+
+      for (const decl of ast.program.body) {
+        if (decl.type === 'ExportDefaultDeclaration') {
+          const exp = decl.declaration;
+          //const exp = ast.program.body[2].declaration;
+
+          for (const p of exp.properties) {
+            switch (p.key.name) {
+              case 'targets':
+                p.key.name = 'output';
+                break;
+              case 'entry':
+                p.key.name = 'input';
+            }
+          }
+          console.log(exp.properties);
+          break;
+        }
+      }
 
       const content = recast.print(ast).code;
 
