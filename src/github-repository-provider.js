3ea1444550e62e51d5465d90ee682d5b8fa95f67
Markus Felten <Markus.Felten@gmx.de>
2017-09-01 23:23:12 +0200

    wip

--- src/github-repository-provider.js
+++ src/github-repository-provider.js
@@ -180,18 +180,20 @@ export class GithubBranch extends Branch {
 
   content(path, options = {}) {
     return new Promise((resolve, reject) =>
-      this.repository.client.repo(repo).contents(path, (err, status, body) => {
-        if (err) {
-          if (options.ignoreMissing) {
-            resolve('');
+      this.repository.client
+        .repo(this.repository.name)
+        .contents(path, (err, status, body) => {
+          if (err) {
+            if (options.ignoreMissing) {
+              resolve('');
+            } else {
+              reject(new Error(`${path}: ${err}`));
+            }
           } else {
-            reject(new Error(`${path}: ${err}`));
+            const b = Buffer.from(status.content, 'base64');
+            resolve(b.toString());
           }
-        } else {
-          const b = Buffer.from(status.content, 'base64');
-          resolve(b.toString());
-        }
-      })
+        })
     );
   }
 }
