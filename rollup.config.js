4221b65794756d4dfb61cd370ae33ff9f5577b73
Markus Felten <Markus.Felten@gmx.de>
2017-09-24 00:16:01 +0200

    chore(rollup): update from template chore(rollup): update from template chore(scripts): update prepare@rollup -c from template chore(devDependencies): update nyc@^11.2.1 from template chore(devDependencies): update rollup@^0.50.0 from template chore(devDependencies): update rollup-plugin-commonjs@^8.2.1 from template chore(devDependencies): update semantic-release@^8.0.3 from template chore(engines): update node@>=8.5.0 from template docs(README): update from template chore(travis): merge from template .travis.yml

--- rollup.config.js
+++ rollup.config.js
@@ -3,11 +3,12 @@ import commonjs from 'rollup-plugin-commonjs';
 import pkg from './package.json';
 
 export default {
-  banner: '#!/usr/bin/env node',
-  targets: [{
-    dest: pkg.bin['npm-template-sync'],
-    format: 'cjs'
-  }],
+  output: {
+    file: pkg.bin['npm-template-sync'],
+    format: 'cjs',
+    banner: '#!/usr/bin/env node'
+  },
   plugins: [nodeResolve(), commonjs()],
-  external: []
+  external: [],
+  input: pkg.module
 };
