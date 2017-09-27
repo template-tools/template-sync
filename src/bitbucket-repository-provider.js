f8ea644ceaa39fcb182627dda1bdd9da635b0e05
Markus Felten <Markus.Felten@gmx.de>
2017-09-27 17:30:01 +0200

    feat: bitbucket branch list

--- src/bitbucket-repository-provider.js
+++ src/bitbucket-repository-provider.js
@@ -1,5 +1,7 @@
 import { Provider, Repository, Branch } from './repository-provider';
 
+const { Client } = require('bitbucket-server-nodejs');
+
 export class BitbucketProvider extends Provider {
   static get repositoryClass() {
     return BitbucketRepository;
@@ -9,14 +11,37 @@ export class BitbucketProvider extends Provider {
     return BitbucketBranch;
   }
 
-  constructor(options = {}) {
+  constructor(auth = {}) {
     super();
+    Object.defineProperty(this, 'client', {
+      value: new Client('https://api.bitbucket.org/2.0', auth)
+    });
   }
 }
 
 export class BitbucketRepository extends Repository {
   constructor(provider, name) {
-    super(provider, name);
+    super(provider, name.replace(/#.*/, ''));
+    Object.defineProperty(this, 'user', { value: name.split(/\//)[0] });
+  }
+
+  get client() {
+    return this.provider.client;
+  }
+
+  async branches() {
+    const res = await this.client.get(
+      `repositories/${this.name}/refs/branches`
+    );
+
+    //console.log(res);
+
+    res.values.forEach(b => {
+      const branch = new this.provider.constructor.branchClass(this, b.name);
+      this._branches.set(branch.name, branch);
+    });
+
+    return this._branches;
   }
 }
 
--- tests/bitbucket-repository-provider-test.js
+++ tests/bitbucket-repository-provider-test.js
@@ -1,12 +1,23 @@
 import test from 'ava';
 import { BitbucketProvider } from '../src/bitbucket-repository-provider';
 
-const REPOSITORY_URL = 'https://arlac77@bitbucket.org/arlac77/sync-test-repository.git';
+const REPOSITORY_URL =
+  'https://arlac77@bitbucket.org/arlac77/sync-test-repository.git';
 const REPOSITORY_NAME = 'arlac77/sync-test-repository';
 
 test('provider', async t => {
-  const provider = new BitbucketProvider();
+  const provider = new BitbucketProvider({
+    password: process.env.BITBUCKET_PASSWORD,
+    user: process.env.BITBUCKET_USER
+  });
+
   const repository = await provider.repository(REPOSITORY_NAME);
 
   t.is(repository.name, REPOSITORY_NAME);
+
+  const branches = await repository.branches();
+  t.is(branches.get('master').name, 'master');
+
+  const branch = await repository.branch('master');
+  t.is(branch.name, 'master');
 });
