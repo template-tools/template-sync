d37afc7946de6967c1c5391fe144b11b8d042ec4
Markus Felten <Markus.Felten@gmx.de>
2017-10-08 16:41:51 +0200

    test(Context): check for several usedDevModules

--- tests/context-test.js
+++ tests/context-test.js
@@ -1,6 +1,7 @@
 import test from 'ava';
 import Context from '../src/context';
 import Rollup from '../src/rollup';
+import Package from '../src/package';
 
 import { MockProvider } from './repository-mock';
 
@@ -15,11 +16,21 @@ export default {
   }
 };`;
 
+const PACKAGE_FILE_CONTENT = `{
+  "release": {
+    "verifyRelease": "cracks"
+  }
+}`;
+
 test('context used dev modules', async t => {
   const provider = new MockProvider({
     'rollup.config.js': {
       templateRepo: ROLLUP_FILE_CONTENT,
       targetRepo: ROLLUP_FILE_CONTENT
+    },
+    'package.json': {
+      templateRepo: PACKAGE_FILE_CONTENT,
+      targetRepo: PACKAGE_FILE_CONTENT
     }
   });
 
@@ -29,8 +40,12 @@ test('context used dev modules', async t => {
   );
 
   context.addFile(new Rollup('rollup.config.js'));
+  context.addFile(new Package('package.json'));
 
-  t.deepEqual(await context.usedDevModules(), new Set(['rollup-plugin-babel']));
+  t.deepEqual(
+    await context.usedDevModules(),
+    new Set(['rollup-plugin-babel', 'cracks'])
+  );
 });
 
 test('context optional dev modules', async t => {
