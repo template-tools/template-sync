8fe74ed0334037f15367e0ed4fdc5dbc95c0abb7
Markus Felten <Markus.Felten@gmx.de>
2017-10-08 18:34:19 +0200

    test(package): chc usedDevModules to

--- tests/package-module-test.js
+++ tests/package-module-test.js
@@ -13,3 +13,17 @@ test('optional dev modules empty', t => {
   const pkg = new Package('package.json');
   t.deepEqual(pkg.optionalDevModules(new Set()), new Set());
 });
+
+const PACKAGE_FILE_CONTENT = `{
+  "release": {
+    "verifyRelease": "cracks"
+  }
+}`;
+
+test('used dev modules', async t => {
+  const pkg = new Package('package.json');
+  t.deepEqual(
+    await pkg.usedDevModules(PACKAGE_FILE_CONTENT),
+    new Set(['cracks'])
+  );
+});
