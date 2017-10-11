cdde8288372d531b0f7f177e1f6c8fbc9085cc60
Markus Felten <Markus.Felten@gmx.de>
2017-10-11 23:45:31 +0200

    feat: automatically write password into keytar

--- src/npm-template-sync.js
+++ src/npm-template-sync.js
@@ -22,11 +22,10 @@ program
   .option(
     '-k, --keystore <account/service>',
     'keystore',
-    /^[\w\-]+\/.*/,
-    'arlac77/GitHub for Mac SSH key passphrase — github.com'
+    /\w+(\/\w+)?/,
+    'github/GH_TOKEN'
   )
   //  .option('-d, --dry', 'dry run do not change anything')
-  .option('-s, --save', 'save keystore')
   .option(
     '-t, --template <user/repo>',
     'template repository',
@@ -34,65 +33,52 @@ program
   )
   .argument('[repos...]', 'repos to merge')
   .action(async (args, options) => {
-    const keystore = {};
-    [keystore.account, keystore.service] = options.keystore.split(/\//);
+    const password = await getPassword(options);
 
     spinner.start();
 
-    const pass = await keytar.findPassword(keystore.service);
-
-    //const pass = await keytar.getPassword(keystore.account, keystore.service);
-
     await Promise.all(
       args.repos.map(repo =>
-        worker(spinner, pass, repo, options.template, options.dry)
+        worker(spinner, password, repo, options.template, options.dry)
       )
     );
-
-    /*
-    if (options.save) {
-      const schema = {
-        properties: {
-          password: {
-            required: true,
-            hidden: true
-          }
-        }
-      };
-      prompt.get(schema, (err, result) => {
-        if (err) {
-          spinner.fail(err);
-          return;
-        }
-        keychain.setPassword(
-          {
-            account: keystore.account,
-            service: keystore.service,
-            password: result.password
-          },
-          err => {
-            if (err) {
-              spinner.fail(err);
-              return;
-            }
-            spinner.succeed('password set');
-          }
-        );
-      });
-    }
-
-    keychain.getPassword(keystore, (err, pass) => {
-      if (err) {
-        spinner.fail(err);
-        return;
-      }
-      Promise.all(
-        args.repos.map(repo =>
-          worker(spinner, pass, repo, options.template, options.dry)
-        )
-      );
-    });
-*/
   });
 
 program.parse(process.argv);
+
+async function getPassword(options) {
+  let [account, service] = options.keystore.split(/\//);
+
+  const password = await keytar.getPassword(account, service);
+
+  //const password = await keytar.findPassword(service);
+
+  if (password !== null) {
+    return password;
+  }
+
+  return await savePassword(account, service);
+}
+
+function savePassword(account, service) {
+  const schema = {
+    properties: {
+      password: {
+        required: true,
+        hidden: true
+      }
+    }
+  };
+
+  return new Promise((resolve, reject) =>
+    prompt.get(schema, (err, result) => {
+      if (err) {
+        console.log(err);
+        reject(err);
+        return;
+      }
+      keytar.setPassword(account, service, result.password);
+      resolve(result.password);
+    })
+  );
+}
