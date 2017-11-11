7cce9e72957d3f53d172ca76ea92e26f219cdfb6
Markus Felten <Markus.Felten@gmx.de>
2017-11-11 12:36:40 +0100

    test: use temp workspace dir

--- tests/local-repository-provider-test.js
+++ tests/local-repository-provider-test.js
@@ -2,15 +2,15 @@ import test from 'ava';
 import { LocalProvider } from '../src/local-repository-provider';
 
 const path = require('path');
+const tempy = require('tempy');
 
-const workspace = path.join(__dirname, '..', 'build', 'sample_repo');
-
-const config = { workspace };
+//const workspace = path.join(__dirname, '..', 'build', 'sample_repo');
+//const config = { workspace };
 
 const REPOSITORY_NAME = 'https://github.com/arlac77/sync-test-repository.git';
 
 test('local provider', async t => {
-  const provider = new LocalProvider(config);
+  const provider = new LocalProvider({ workspace: tempy.directory() });
 
   const repository = await provider.repository(REPOSITORY_NAME);
 
@@ -18,7 +18,7 @@ test('local provider', async t => {
 });
 
 test('local provider create branch', async t => {
-  const provider = new LocalProvider(config);
+  const provider = new LocalProvider({ workspace: tempy.directory() });
   const repository = await provider.repository(REPOSITORY_NAME);
   const branches = await repository.branches();
 
@@ -32,7 +32,7 @@ test('local provider create branch', async t => {
 });
 
 test.skip('local provider list files', async t => {
-  const provider = new LocalProvider(config);
+  const provider = new LocalProvider({ workspace: tempy.directory() });
   const repository = await provider.repository(REPOSITORY_NAME);
   const branch = await repository.branch('master');
 
