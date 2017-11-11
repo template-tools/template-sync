25cebe25c7232fb1823ab86006086f8d2d65ee31
Markus Felten <Markus.Felten@gmx.de>
2017-11-11 12:22:10 +0100

    feat: ongoing local repo provider work

--- src/local-repository-provider.js
+++ src/local-repository-provider.js
@@ -25,9 +25,49 @@ export class LocalRepository extends Repository {
   async initialize() {
     try {
       await stat(this.workspace);
+      const result = await execa('git', ['pull'], { cwd: this.workspace });
     } catch (e) {
       const result = await execa('git', ['clone', this.name, this.workspace]);
     }
+  }
+
+  async branches() {
+    const result = await execa('git', ['branch'], {
+      cwd: this.workspace
+    });
+
+    result.stdout.split(/\n/).forEach(b => {
+      const m = b.match(/^\*?\s*([^\s]+)/);
+      if (m) {
+        const name = m[1];
+        const branch = new this.provider.constructor.branchClass(this, name);
+        this._branches.set(branch.name, branch);
+      }
+    });
+
+    return this._branches;
+  }
+
+  async createBranch(name, from) {
+    const result = await execa('git', ['checkout', '-b', name], {
+      cwd: this.workspace
+    });
+
+    const b = new this.provider.constructor.branchClass(this, name);
+    this._branches.set(b.name, b);
+    return b;
+  }
+
+  async deleteBranch(name) {
+    await execa('git', ['checkout', 'master'], {
+      cwd: this.workspace
+    });
+
+    const result = await execa('git', ['branch', '-D', name], {
+      cwd: this.workspace
+    });
+
+    this._branches.delete(name);
   }
 }
 
--- tests/local-repository-provider-test.js
+++ tests/local-repository-provider-test.js
@@ -3,16 +3,40 @@ import { LocalProvider } from '../src/local-repository-provider';
 
 const path = require('path');
 
+const workspace = path.join(__dirname, '..', 'build', 'sample_repo');
+
+const config = { workspace };
+
+const REPOSITORY_NAME = 'https://github.com/arlac77/sync-test-repository.git';
+
 test('local provider', async t => {
-  const dir = path.join(__dirname, '..', 'build', 'sample_repo');
+  const provider = new LocalProvider(config);
 
-  const provider = new LocalProvider({
-    workspace: dir
-  });
+  const repository = await provider.repository(REPOSITORY_NAME);
 
-  const repository = await provider.repository(
-    'https://github.com/arlac77/sync-test-repository.git'
-  );
+  t.is(repository.name, REPOSITORY_NAME);
+});
 
-  t.is(repository.name, 'https://github.com/arlac77/sync-test-repository.git');
+test('local provider create branch', async t => {
+  const provider = new LocalProvider(config);
+  const repository = await provider.repository(REPOSITORY_NAME);
+  const branches = await repository.branches();
+
+  const newName = `test-${branches.size}`;
+  const branch = await repository.createBranch(newName);
+
+  t.is(branch.name, newName);
+
+  await repository.deleteBranch(newName);
+  t.is(branches.get(newName), undefined);
+});
+
+test.skip('local provider list files', async t => {
+  const provider = new LocalProvider(config);
+  const repository = await provider.repository(REPOSITORY_NAME);
+  const branch = await repository.branch('master');
+
+  const files = await branch.list();
+
+  t.is(files[0].path, 'README.md');
 });
