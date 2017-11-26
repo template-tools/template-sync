30527104562674500d1412c8389a941e07711241
Markus Felten <Markus.Felten@gmx.de>
2017-11-26 19:11:48 +0100

    refactor: prepare for keytar

--- src/util.js
+++ src/util.js
@@ -23,3 +23,34 @@ export function setPassword(...args) {
     });
   });
 }
+
+/*
+const keytar = require('keytar');
+
+export async function getPassword(options) {
+  const [account, service] = options.keystore.split(/\//);
+
+  const password = await keytar.getPassword(account, service);
+
+  if (password !== null) {
+    return password;
+  }
+
+  return savePassword(account, service);
+}
+
+export async function savePassword(account, service) {
+  const schema = {
+    properties: {
+      password: {
+        required: true,
+        hidden: true
+      }
+    }
+  };
+
+  keytar.setPassword(account, service, password);
+
+  return password;
+}
+*/
