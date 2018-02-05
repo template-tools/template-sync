ceae1def1459e407ce7b8dd100b2c54582613188
Markus Felten <Markus.Felten@gmx.de>
2018-02-05 20:23:49 +0100

    fix: handle missing keytar

--- src/util.js
+++ src/util.js
@@ -1,17 +1,19 @@
-const keytar = require('keytar');
-
 export async function getPassword(options) {
   const [account, service] = options.keystore.split(/\//);
 
-  const password = await keytar.getPassword(account, service);
-
-  return password;
+  try {
+    const { getPassword } = require('keytar');
+    return getPassword(account, service);
+  } catch (e) {}
+  return undefined;
 }
 
 export async function setPassword(pasword, options) {
   const [account, service] = options.keystore.split(/\//);
-
-  keytar.setPassword(account, service, password);
+  try {
+    const { setPassword } = require('keytar');
+    setPassword(account, service, password);
+  } catch (e) {}
 
   return password;
 }
