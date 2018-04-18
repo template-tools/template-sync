f78cedb7d9d5766c45996c0d1486ffdb22814805
Markus Felten <Markus.Felten@gmx.de>
2018-04-18 20:31:56 +0200

    chore: use rollup-plugin-executable

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
+import executable from 'rollup-plugin-executable';
 import json from 'rollup-plugin-json';
 import pkg from './package.json';
 
@@ -23,7 +24,7 @@ export default [
       format: 'cjs',
       banner: '#!/usr/bin/env node'
     },
-    plugins: [nodeResolve(), commonjs(), json()],
+    plugins: [nodeResolve(), commonjs(), json(), executable()],
     external,
     input: 'src/npm-template-sync-cli.js'
   },
