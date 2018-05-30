45511bef40a38d5886489d46f37afbed6d6df916
Markus Felten <Markus.Felten@gmx.de>
2018-05-30 20:23:21 +0200

    test: add cli dry run

--- tests/cli-test.js
+++ tests/cli-test.js
@@ -12,3 +12,13 @@ test('cli defines', async t => {
   ]);
   t.truthy(c.stdout.match(/a new module/));
 });
+
+test('cli dryrun', async t => {
+  const c = await execa(nts, ['--dry', 'true', 'arlac77/config-expander']);
+
+  /*
+  console.log(c.stdout);
+  console.log(c.stderr);
+*/
+  t.is(c.code, 0);
+});
