36f95eea7a395ca00e71d835fa0d0ec7407e5ec8
Markus Felten <Markus.Felten@gmx.de>
2018-07-17 00:11:42 +0200

    chore(package): add rollup-plugin-istanbul@^2.0.1 chore(rollup): import resolve chore(rollup): import resolve chore(rollup): import istanbul chore(rollup): add multiEntry to plugins chore(rollup): add istanbul to plugins chore(rollup): add resolve to plugins chore(rollup): add commonjs to plugins

--- tests/rollup.config.js
+++ tests/rollup.config.js
@@ -1,3 +1,6 @@
+import istanbul from 'rollup-plugin-istanbul';
+
+import resolve from 'rollup-plugin-node-resolve';
 import multiEntry from 'rollup-plugin-multi-entry';
 import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
