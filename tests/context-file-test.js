8f75a87938c5937231d508aa9518b0f87a72b12b
Markus Felten <Markus.Felten@gmx.de>
2018-09-02 10:56:23 +0200

    test: PreparedContext.createFiles() with from env initialized GithubProvider

--- tests/context-file-test.js
+++ tests/context-file-test.js
@@ -1,42 +1,44 @@
-import test from 'ava';
-import { Context } from '../src/context';
-import { PreparedContext } from '../src/prepared-context';
-import { MockProvider } from 'mock-repository-provider';
-import { GithubProvider } from 'github-repository-provider';
-import { Package } from '../src/package';
-import { Rollup } from '../src/rollup';
+import test from "ava";
+import { Context } from "../src/context";
+import { PreparedContext } from "../src/prepared-context";
+import { MockProvider } from "mock-repository-provider";
+import { GithubProvider } from "github-repository-provider";
+import { Package } from "../src/package";
+import { Rollup } from "../src/rollup";
 
-const REPOSITORY_NAME = 'arlac77/sync-test-repository';
+const REPOSITORY_NAME = "arlac77/sync-test-repository";
 
-test('create files', async t => {
-  const provider = new GithubProvider(process.env.GH_TOKEN);
-
-  const files = await PreparedContext.createFiles(
-    await provider.branch(REPOSITORY_NAME)
+test("create files", async t => {
+  const provider = new GithubProvider(
+    GithubProvider.optionsFromEnvironment(process.env)
   );
 
-  t.is(files.find(f => f.path === 'package.json').path, 'package.json');
-  t.is(files.find(f => f.path === 'package.json').constructor.name, 'Package');
+  const files = await PreparedContext.createFiles(
+    await provider.branch(REPOSITORY_NAME + "#master")
+  );
+
+  t.is(files.find(f => f.path === "package.json").path, "package.json");
+  t.is(files.find(f => f.path === "package.json").constructor.name, "Package");
 });
 
-test('context file targetContent', async t => {
+test("context file targetContent", async t => {
   const provider = new MockProvider({
     templateRepo: {
-      master: { 'package.json': '{"name":"a"}' }
+      master: { "package.json": '{"name":"a"}' }
     },
     targetRepo: {
-      master: { 'package.json': '{"name":"b"}' }
+      master: { "package.json": '{"name":"b"}' }
     }
   });
 
   const context = await PreparedContext.from(
     new Context(provider, {
-      templateBranchName: 'templateRepo'
+      templateBranchName: "templateRepo"
     }),
-    'targetRepo'
+    "targetRepo"
   );
 
-  const f = new Package('package.json');
+  const f = new Package("package.json");
 
   t.is(await f.targetContent(context), '{"name":"b"}');
 });
@@ -58,59 +60,59 @@ const PACKAGE_FILE_CONTENT = `{
   }
 }`;
 
-test('context used dev modules', async t => {
+test("context used dev modules", async t => {
   const provider = new MockProvider({
     templateRepo: {
       master: {
-        'rollup.config.js': ROLLUP_FILE_CONTENT,
-        'package.json': PACKAGE_FILE_CONTENT
+        "rollup.config.js": ROLLUP_FILE_CONTENT,
+        "package.json": PACKAGE_FILE_CONTENT
       }
     },
     targetRepo: {
       master: {
-        'rollup.config.js': ROLLUP_FILE_CONTENT,
-        'package.json': PACKAGE_FILE_CONTENT
+        "rollup.config.js": ROLLUP_FILE_CONTENT,
+        "package.json": PACKAGE_FILE_CONTENT
       }
     }
   });
 
   const context = await PreparedContext.from(
     new Context(provider, {
-      templateBranchName: 'templateRepo'
+      templateBranchName: "templateRepo"
     }),
-    'targetRepo'
+    "targetRepo"
   );
 
-  context.addFile(new Rollup('rollup.config.js'));
-  context.addFile(new Package('package.json'));
+  context.addFile(new Rollup("rollup.config.js"));
+  context.addFile(new Package("package.json"));
 
   t.deepEqual(
     await context.usedDevModules(),
-    new Set(['rollup-plugin-babel', 'cracks'])
+    new Set(["rollup-plugin-babel", "cracks"])
   );
 });
 
-test('context optional dev modules', async t => {
+test("context optional dev modules", async t => {
   const provider = new MockProvider({
-    templateRepo: { master: { 'rollup.config.js': ROLLUP_FILE_CONTENT } },
+    templateRepo: { master: { "rollup.config.js": ROLLUP_FILE_CONTENT } },
     targetRepo: {
       master: {
-        'rollup.config.js': ROLLUP_FILE_CONTENT
+        "rollup.config.js": ROLLUP_FILE_CONTENT
       }
     }
   });
 
   const context = await PreparedContext.from(
     new Context(provider, {
-      templateBranchName: 'templateRepo'
+      templateBranchName: "templateRepo"
     }),
-    'targetRepo'
+    "targetRepo"
   );
 
-  context.addFile(new Rollup('rollup.config.js'));
+  context.addFile(new Rollup("rollup.config.js"));
 
   t.deepEqual(
-    context.optionalDevModules(new Set(['rollup-plugin-babel'])),
-    new Set(['rollup-plugin-babel'])
+    context.optionalDevModules(new Set(["rollup-plugin-babel"])),
+    new Set(["rollup-plugin-babel"])
   );
 });
