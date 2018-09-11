ed3c48c827a5990febc2d3959331ef59e1c56b24
Markus Felten <Markus.Felten@gmx.de>
2018-09-11 23:20:51 +0200

    chore(rollup): reenable istanbul

--- tests/rollup.config.js
+++ tests/rollup.config.js
@@ -1,52 +1,49 @@
-import istanbul from 'rollup-plugin-istanbul';
-import resolve from 'rollup-plugin-node-resolve';
-import multiEntry from 'rollup-plugin-multi-entry';
-import commonjs from 'rollup-plugin-commonjs';
-import json from 'rollup-plugin-json';
-import pkg from '../package.json';
-import baseRollup from '../rollup.config.js';
+import istanbul from "rollup-plugin-istanbul";
+import resolve from "rollup-plugin-node-resolve";
+import multiEntry from "rollup-plugin-multi-entry";
+import commonjs from "rollup-plugin-commonjs";
+import baseRollup from "../rollup.config.js";
 
 const external = [
-  'ava',
-  'os',
-  'path',
-  'crypto',
-  'fs',
-  'util',
-  'url',
-  'net',
-  'tty',
-  'mock-repository-provider',
-  'execa',
-  'jsonpath',
-  'deep-extend',
-  'simple-diff',
-  'micromatch',
-  'local-repository-provider',
-  'aggregation-repository-provider',
-  'github-repository-provider',
-  'bitbucket-repository-provider',
-  'expression-expander'
+  "ava",
+  "os",
+  "path",
+  "crypto",
+  "fs",
+  "util",
+  "url",
+  "net",
+  "tty",
+  "mock-repository-provider",
+  "execa",
+  "jsonpath",
+  "deep-extend",
+  "simple-diff",
+  "micromatch",
+  "local-repository-provider",
+  "aggregation-repository-provider",
+  "github-repository-provider",
+  "bitbucket-repository-provider",
+  "expression-expander"
 ];
 
 export default [
-  //  ...baseRollup,
+  ...baseRollup,
   {
-    input: 'tests/**/*-test.js',
+    input: "tests/**/*-test.js",
     external,
     plugins: [
-      multiEntry() //,
-      //      istanbul({
-      //        exclude: ['tests/**/*-test.js', 'node_modules/**/*']
-      /*    }),
+      multiEntry(),
       resolve(),
-      commonjs()
-      */
+      commonjs(),
+      istanbul({
+        exclude: ["tests/**/*-test.js", "node_modules/**/*"]
+      })
     ],
 
     output: {
-      file: 'build/bundle-test.js',
-      format: 'cjs',
+      file: "build/bundle-test.js",
+      format: "cjs",
       sourcemap: true,
       interop: false
     }
