9ceb9ff79cb7a0caa65d1784cbd29f5f0a35a3eb
Markus Felten <Markus.Felten@gmx.de>
2018-09-13 16:18:07 +0200

    fix(rollup): generate node call with --experimental-modules --experimental-worker

--- rollup.config.js
+++ rollup.config.js
@@ -1,47 +1,48 @@
-import cleanup from 'rollup-plugin-cleanup';
-import resolve from 'rollup-plugin-node-resolve';
-import nodeResolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import executable from 'rollup-plugin-executable';
-import json from 'rollup-plugin-json';
-import pkg from './package.json';
+import cleanup from "rollup-plugin-cleanup";
+import resolve from "rollup-plugin-node-resolve";
+import nodeResolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import executable from "rollup-plugin-executable";
+import json from "rollup-plugin-json";
+import pkg from "./package.json";
 
 const external = [
-  'os',
-  'path',
-  'crypto',
-  'fs',
-  'util',
-  'url',
-  'net',
-  'tty',
-  'jsonpath',
-  'deep-extend',
-  'simple-diff',
-  'micromatch',
-  'local-repository-provider',
-  'aggregation-repository-provider',
-  'github-repository-provider',
-  'bitbucket-repository-provider',
-  'expression-expander'
+  "os",
+  "path",
+  "crypto",
+  "fs",
+  "util",
+  "url",
+  "net",
+  "tty",
+  "jsonpath",
+  "deep-extend",
+  "simple-diff",
+  "micromatch",
+  "local-repository-provider",
+  "aggregation-repository-provider",
+  "github-repository-provider",
+  "bitbucket-repository-provider",
+  "expression-expander"
 ];
 
 export default [
   {
     output: {
-      file: pkg.bin['npm-template-sync'],
-      format: 'cjs',
-      banner: '#!/usr/bin/env node',
+      file: pkg.bin["npm-template-sync"],
+      format: "cjs",
+      banner:
+        "#!/usr/bin/env node --experimental-modules --experimental-worker",
       interop: false
     },
     plugins: [nodeResolve(), commonjs(), json(), cleanup(), executable()],
     external,
-    input: 'src/npm-template-sync-cli.js'
+    input: "src/npm-template-sync-cli.js"
   },
   {
     output: {
       file: pkg.main,
-      format: 'cjs',
+      format: "cjs",
       interop: false
     },
     plugins: [nodeResolve(), commonjs(), cleanup()],
