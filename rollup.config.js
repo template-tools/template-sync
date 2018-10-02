1b10ea2ab6de190a0e72ebd4c9ec8fca696617ac
Markus Felten <Markus.Felten@gmx.de>
2018-10-02 15:03:28 +0200

    fix(cli): make env working on linux too

--- rollup.config.js
+++ rollup.config.js
@@ -31,7 +31,8 @@ export default [
     output: {
       file: pkg.bin["npm-template-sync"],
       format: "cjs",
-      banner: "#!/usr/bin/env node",
+      banner:
+        '#!/bin/sh\n":" //# comment; exec /usr/bin/env node --experimental-modules --experimental-worker "$0" "$@"',
       interop: false
     },
     plugins: [
