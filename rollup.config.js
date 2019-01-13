3889360fd7381da3c53a63761d0f6437cd2af596
Markus Felten <Markus.Felten@gmx.de>
2019-01-13 21:30:02 +0100

    chore(package): ava@^1.1.0 chore(package): rollup-plugin-executable@^1.4.3 chore(package): semantic-release@^15.13.3 chore(scripts): cover@#overwrite c8 --temp-directory build/coverage ava && c8 report -r lcov --temp-directory build/coverage chore(package): add nyc from template chore(package): set $.ava.require='esm' as in template chore(package): set $.ava.files='tests/*-test.js,tests/*-test.mjs' as in template chore(package): set $.ava.extensions='js,mjs' as in template chore(rollup): import cleanup

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+import cleanup from "rollup-plugin-cleanup";
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 import executable from "rollup-plugin-executable";
