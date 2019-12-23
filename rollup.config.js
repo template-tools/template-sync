e5c51d7d9a67a525a897a18198e4136c04c79140
Markus Felten <Markus.Felten@gmx.de>
2019-12-24 00:35:23 +0100

    fix(rollup): use @rollup/plugin-commonjs

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import resolve from "@rollup/plugin-node-resolve";
 import json from "@rollup/plugin-json";
+import commonjs from "@rollup/plugin-commonjs";
 
-import commonjs from "rollup-plugin-commonjs";
 import executable from "rollup-plugin-executable";
 import cleanup from "rollup-plugin-cleanup";
 import builtins from "builtin-modules";
