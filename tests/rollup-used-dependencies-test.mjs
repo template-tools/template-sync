6dbfbb0ffe2323e5cf07b34334794d22bafb8d15
Markus Felten <Markus.Felten@gmx.de>
2020-02-11 17:37:38 +0100

    test: additional rollup usedDevModules

--- tests/rollup-used-dependencies-test.mjs
+++ tests/rollup-used-dependencies-test.mjs
@@ -1,10 +1,19 @@
 import test from "ava";
 import { Rollup } from "../src/mergers/rollup.mjs";
 
-test("rollup used dependencies", async t => {
+async function rudt(t, content, dependencies) {
   const rollup = new Rollup("rollup.config.json");
+  const modules = rollup.usedDevDependencies(content);
 
-  const modules = rollup.usedDevDependencies(`import babel from 'rollup-plugin-babel';
+  t.deepEqual(modules, new Set(dependencies));
+}
+
+rudt.title = (providedTitle = "", content, dependencies) =>
+  `rollup used dependencies ${providedTitle} ${content} ${dependencies}`.trim();
+
+test(
+  rudt,
+  `import babel from 'rollup-plugin-babel';
   import multiEntry from 'rollup-plugin-multi-entry';
   import resolve from "@rollup/plugin-node-resolve";
 
@@ -22,17 +31,66 @@ test("rollup used dependencies", async t => {
     format: 'cjs',
     dest: 'build/test-bundle.js',
     sourceMap: true
-  };`);
+  };`,
+  [
+    "rollup-plugin-babel",
+    "rollup-plugin-multi-entry",
+    "@rollup/plugin-node-resolve"
+  ]
+);
 
-  t.deepEqual(
-    modules,
-    new Set([
-      "rollup-plugin-babel",
-      "rollup-plugin-multi-entry",
-      "@rollup/plugin-node-resolve"
-    ])
-  );
-});
+test(
+  rudt,
+  `import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import svelte from "rollup-plugin-svelte";
+import { terser } from "rollup-plugin-terser";
+import dev from "rollup-plugin-dev";
+import copy from "rollup-plugin-copy";
+import consts from "rollup-plugin-consts";
+import { name, description, version, config } from "./package.json";
+
+const dist = "public";
+const port = 5000;
+
+export default {
+  input: "src/main.mjs",
+  output: {
+    sourcemap: true,
+    format: "esm",
+  },
+  plugins: [
+    consts({
+      name,
+      version,
+      description,
+      ...config
+    }),
+    svelte({
+      css: css => {
+      }
+    }),
+    resolve({ browser: true }),
+    commonjs(),
+    production && terser(),
+    dev({
+      port,
+      dirs: [dist],
+      basePath: config.base
+    })
+  ]
+};`,
+  [
+    "@rollup/plugin-node-resolve",
+    "@rollup/plugin-commonjs",
+    "rollup-plugin-svelte",
+    "rollup-plugin-terser",
+    "rollup-plugin-dev",
+    "rollup-plugin-copy",
+    "rollup-plugin-consts",
+    "./package.json"
+  ]
+);
 
 test("rollup optional dev modules", t => {
   const file = new Rollup("rollup.config.json");
