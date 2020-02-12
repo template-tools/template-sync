d6a0d1262aa2d93935478f79b9f680dc9b9d24ac
Markus Felten <Markus.Felten@gmx.de>
2020-02-12 03:44:02 +0100

    feat(json): implement new static merge()

--- src/mergers/json.mjs
+++ src/mergers/json.mjs
@@ -1,11 +1,43 @@
+import { StringContentEntry } from "content-entry";
 import { merge } from "hinted-tree-merger";
-import { actions2messages, aggregateActions } from "../util.mjs";
+import { actions2messages, actions2message, aggregateActions } from "../util.mjs";
 import { Merger } from "../merger.mjs";
 
 export class JSONMerger extends Merger {
 
   static get pattern() {
     return "**/*.json";
+  }
+
+  static async merge(
+    context,
+    destinationEntry,
+    sourceEntry,
+    options = YAML.defaultOptions
+  ) {
+    const name = destinationEntry.name;
+    const original = await destinationEntry.getString();
+    const template = await sourceEntry.getString();
+
+    const actions = {};
+    
+    return {
+      message: actions2message(actions, options.messagePrefix, name),
+      entry: new StringContentEntry(
+        name,
+        yaml.safeDump(
+          merge(
+            JSON.parse(original) || {},
+            JSON.parse(
+              options.expand ? context.expand(template) : template
+            ),
+            "",
+            (action, hint) => aggregateActions(actions, action, hint),
+            options.mergeHints
+          )
+        )
+      )
+    };
   }
 
   async mergeContent(context, original, template) {
