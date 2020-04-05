218de4fd858c1ec9d3a7a2286fee6f9a5293050e
Markus Felten <Markus.Felten@gmx.de>
2020-04-05 22:04:19 +0200

    test: replace-if-empty

---
+++ tests/replace-if-empty-test.mjs
@@ -0,0 +1,25 @@
+import test from "ava";
+import { createContext } from "./helpers/util.mjs";
+import { StringContentEntry, EmptyContentEntry } from "content-entry";
+
+import { ReplaceIfEmpty } from "../src/mergers/replace-if-empty.mjs";
+
+test("replace-if-empty differ", async t => {
+  const merged = await ReplaceIfEmpty.merge(
+    await createContext(),
+    new EmptyContentEntry("aFile"),
+    new StringContentEntry("aFile", "Line 1x")
+  );
+
+  t.is(await merged.entry.getString(), "Line 1x");
+});
+
+test("replace-if-empty nop", async t => {
+  const merged = await ReplaceIfEmpty.merge(
+    await createContext(),
+    new StringContentEntry("aFile", "Line 1"),
+    new StringContentEntry("aFile", "Line 1")
+  );
+
+  t.is(merged, undefined);
+});
