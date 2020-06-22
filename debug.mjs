9bd7832ea133b7fa09fa26739c973d0eebded27c
Markus Felten <Markus.Felten@db.com>
2020-06-22 10:20:16 +0200

    chore: cleanup

--- debug.mjs
+++
@@ -1,9 +0,0 @@
-import { mergeTemplate } from "./src/template.mjs";
-import { readFileSync } from "fs";
-
-console.log(
-  mergeTemplate(
-    JSON.parse(readFileSync("8-left.json", { encoding: "utf8" })),
-    JSON.parse(readFileSync("8-right.json", { encoding: "utf8" }))
-  )
-);
