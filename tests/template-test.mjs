a16f7733eea2820998e7d2cc6b4bd41701837101
Markus Felten <Markus.Felten@gmx.de>
2020-11-20 11:25:02 +0100

    test: add assert messages

--- tests/template-test.mjs
+++ tests/template-test.mjs
@@ -53,14 +53,14 @@ async function tt(t, sources, key) {
   const template = await new Template(context, sources);
   t.true(template instanceof Template);
 
-  t.deepEqual(template.sources, sources);
-  t.is(`${template}`, sources.join(','));
-  t.is(template.name, sources.join(','));
-  t.is(template.key, key);
+  t.deepEqual(template.sources, sources, "sources");
+  t.is(`${template}`, sources.join(','), "toString");
+  t.is(template.name, sources.join(','), "name");
+  t.is(template.key, key, "key");
 }
 
 tt.title = (providedTitle = "", sources, key) =>
-  `Template ${providedTitle}${sources} '${key}'`.trim();
+  `Template key ${providedTitle}[${sources}] '${key}'`.trim();
 
 test(tt, ["template"], "template,template_b");
 test(tt, ["template_b", "template"], "template,template_b");
