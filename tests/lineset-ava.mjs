6355e227fe1ed28665b3f4d9c3fe4777b19d9a85
Markus Felten <Markus.Felten@gmx.de>
2022-12-31 13:09:05 +0100

    test: MergeLineSet empty

--- tests/lineset-ava.mjs
+++ tests/lineset-ava.mjs
@@ -1,6 +1,6 @@
 import test from "ava";
 import { createContext, asyncIterator2scalar } from "./helpers/util.mjs";
-import { StringContentEntry } from "content-entry";
+import { StringContentEntry, EmptyContentEntry } from "content-entry";
 import { MergeLineSet } from "../src/mergers/merge-line-set.mjs";
 
 test("MergeLineSet lines remove", async t => {
@@ -62,4 +62,29 @@ test("MergeLineSet ignore option", async t => {
   );
 
   t.is(await commit.entries[0].string, "Line 1\n");
+});
+
+test("MergeLineSet from empty", async t => {
+  const commit = await asyncIterator2scalar(
+    MergeLineSet.commits(
+      await createContext(),
+      new StringContentEntry("aFile", ["Line 1", "Line 2"].join("\n") + "\n"),
+      new EmptyContentEntry("aFile")
+    )
+  );
+
+  t.is(commit, undefined);
+});
+
+test("MergeLineSet into empty", async t => {
+
+  const commit = await asyncIterator2scalar(
+    MergeLineSet.commits(
+      await createContext(),
+      new EmptyContentEntry("aFile"),
+      new StringContentEntry("aFile", ["Line 1", "Line 2"].join("\n") + "\n")
+    )
+  );
+
+  t.is(await commit.entries[0].string, ["Line 2", "Line 1"].join("\n") + "\n");
 });
